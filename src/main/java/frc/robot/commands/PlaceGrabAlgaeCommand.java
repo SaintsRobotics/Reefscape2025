70ad8403442665e5ad7c46db9f2aeb70fde449cc
mebrahimaleem <mebrahimaleem@gmail.com>
2025-03-05 12:18:08 -0800

    feat: deadlined based intake/outtake

--- src/main/java/frc/robot/commands/PlaceGrabAlgaeCommand.java
+++ src/main/java/frc/robot/commands/PlaceGrabAlgaeCommand.java
@@ -5,7 +5,10 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.EndEffectorSubsystem;
 import frc.robot.utils.Interlocks;
 
@@ -22,19 +25,19 @@ public class PlaceGrabAlgaeCommand extends SequentialCommandGroup {
   public PlaceGrabAlgaeCommand(EndEffectorSubsystem endEffectorSubsystem, boolean outtake, Interlocks interlocks) {
     if (outtake) { //TODO: tune constants, and make dynamic based on elevator height?
       addCommands(
-        new PivotCommand(endEffectorSubsystem, 0),
-        new TimedCommand(endEffectorSubsystem::outtakeAlgae, 0),
-        new InstantCommand(() -> interlocks.setAlgeaHolding(false)),
-        new PivotCommand(endEffectorSubsystem, 0)
+        //new PivotCommand(endEffectorSubsystem, 0),
+        new ParallelDeadlineGroup(new WaitCommand(0), new StartEndCommand(endEffectorSubsystem::outtakeAlgae, endEffectorSubsystem::stopEffector)),
+        new InstantCommand(() -> interlocks.setAlgeaHolding(false))
+        //new PivotCommand(endEffectorSubsystem, 0)
         );
     }
     else {
       interlocks.setAlgeaHolding(true);
       addCommands(
-        new PivotCommand(endEffectorSubsystem, 0),
-        new TimedCommand(endEffectorSubsystem::intakeAlgae, 0),
-        new InstantCommand(() -> interlocks.setAlgeaHolding(true)),
-        new PivotCommand(endEffectorSubsystem, 0)
+        //new PivotCommand(endEffectorSubsystem, 0),
+        new ParallelDeadlineGroup(new WaitCommand(0), new StartEndCommand(endEffectorSubsystem::intakeAlgae, endEffectorSubsystem::stopEffector)),
+        new InstantCommand(() -> interlocks.setAlgeaHolding(true))
+        //new PivotCommand(endEffectorSubsystem, 0)
         );
       
     }
