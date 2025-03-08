08fedb35059f471f213261f783559151f6ab211b
ProgrammingSR <programming@saintsrobotics.com>
2025-03-07 19:04:40 -0800

    fix: algae does not collide

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -304,7 +304,7 @@ public void initSubsystems() {
                         new ElevatorCommand(ElevatorConstants.kL3Height, m_elevator, m_endEffector)
                     ),
                     new InstantCommand(), () -> m_elevator.getCurrentHeight() >= 11),
-                new PivotCommand(m_endEffector, 1.3),
+                new PivotCommand(m_endEffector, .3 * 2 * Math.PI),
                 new ElevatorCommand(ElevatorConstants.kL1Height, m_elevator, m_endEffector),
                 new PivotCommand(m_endEffector, 2.727)
             ),
@@ -337,7 +337,7 @@ public void initSubsystems() {
                         new ElevatorCommand(ElevatorConstants.kL3Height, m_elevator, m_endEffector)
                     ),
                     new InstantCommand(), () -> m_elevator.getCurrentHeight() >= 11),
-                new PivotCommand(m_endEffector, 1.3),
+                new PivotCommand(m_endEffector, .3 * 2 * Math.PI),
                 new ElevatorCommand(6.1, m_elevator, m_endEffector), 
                 new PivotCommand(m_endEffector, 2.746)),
             () -> m_coralMode));
@@ -370,7 +370,7 @@ public void initSubsystems() {
                         new ElevatorCommand(ElevatorConstants.kL3Height, m_elevator, m_endEffector)
                     ),
                     new InstantCommand(), () -> m_elevator.getCurrentHeight() >= 11),
-                new PivotCommand(m_endEffector, 1.3),
+                new PivotCommand(m_endEffector, .3 * 2 * Math.PI),
                 new ElevatorCommand(11.2, m_elevator, m_endEffector), 
                 new PivotCommand(m_endEffector, 2.672)),
             () -> m_coralMode));
