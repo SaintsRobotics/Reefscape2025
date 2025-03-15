d74dc152ce217d82748fcdd241f49de484716dd6
Programming <programming@saintsrobotics.com>
2025-03-14 17:30:51 -0700

    made haptic strength and duration configurable

--- src/main/java/frc/robot/commands/HapticCommand.java
+++ src/main/java/frc/robot/commands/HapticCommand.java
@@ -7,24 +7,30 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class HapticCommand extends Command {
   private XboxController m_controller;
+  private double m_amount;
+  private double m_time;
   private Timer m_timer = new Timer();
   /** Creates a new UpdateHaptics. */
-  public HapticCommand(XboxController controller) {
+  public HapticCommand(XboxController controller, double time, double amount) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_controller = controller;
+    m_amount = amount;
+    m_time = time;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    SmartDashboard.putBoolean("haptic",true);
     m_timer.reset();
-    m_controller.setRumble(GenericHID.RumbleType.kBothRumble, 1);
+    m_controller.setRumble(GenericHID.RumbleType.kBothRumble, m_amount);
     m_timer.start();
   }
 
@@ -44,6 +50,6 @@ public class HapticCommand extends Command {
   @Override
   public boolean isFinished() {
     
-    return m_timer.get()>0.25;
+    return m_timer.get()>m_time;
   }
 }
